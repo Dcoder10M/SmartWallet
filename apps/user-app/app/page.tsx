853de76a8bdf4245d984d5e61c446e03b94d666c
Dcoder10M <div.singh.rathore.ff@gmail.com>
2024-08-24 22:43:44 +0530

    Prisma init

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@repo/db/client";
+const client=new PrismaClient();
+
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center bg-slate-200 font-mono text-3xl text-slate-700 font-medium tracking-wider">
